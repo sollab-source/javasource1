4b1d790c5e805c4e5c22f87bb62a34023b9ef636
jinkyung <pjky5@studydesk.co.kr>
2025-09-24 15:41:08 +0900

    연산자

--- ch1/src/basic/ScannerEx1.java
+++ ch1/src/basic/ScannerEx1.java
@@ -2,6 +2,9 @@ package basic;
 
 import java.util.Scanner;
 
+// System.in : 키보드
+// System.out : 화면
+
 public class ScannerEx1 {
     public static void main(String[] args) {
         // 키보드에서 입력받기
---
+++ ch1/src/operator/OperatorEx3.java
@@ -0,0 +1,18 @@
+package operator;
+
+public class OperatorEx3 {
+    public static void main(String[] args) {
+        // 비교연산자 : > < >= <= == !=
+        // true, false 결과값
+        // == : 같다
+        // != : 같지 않다
+
+        int a = 10;
+        float f = 10.0f;
+        // 연산 수행 전 피연산자의 타입을 맞춘 후 비교
+        System.out.println(a == f); // true
+        System.out.println(a != f); // false
+        System.out.printf("'A' == 65 = %b\n", 'A' == 65); // false
+
+    }
+}
